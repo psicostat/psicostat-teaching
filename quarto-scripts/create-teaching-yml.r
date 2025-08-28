afe1ae249b69b7ab3448249e01138ebed1723006
EnricoToffalini <enrico.toffalini@unipd.it>
2025-08-28 16:51:30 +0200

    update

--- quarto-scripts/create-teaching-yml.r
+++ quarto-scripts/create-teaching-yml.r
@@ -19,14 +19,18 @@ options(gargle_oauth_email = Sys.getenv("GMAIL"), googledrive_quiet = TRUE)
 
 teaching <- drive_dowload_read(info$teaching)
 
-template <- '- title: "%s"\n  author: %s\n  categories: [%s]\n  description: "description"\n  image: teaching/images/%s\n  link: %s'
+template <- '- title: "%s"\n  author: %s\n  categories: [%s]\n  description: %s\n  image: teaching/images/%s\n  link: %s'
 
 image <- list.files("teaching/images/")
-image_to_insert <- ifelse(
-  teaching$id %in% xfun::sans_ext(image),
-  image,
-  "0.png"
+image_to_insert = vapply(
+  teaching$id,
+  function(id) {
+    match_idx <- match(id, xfun::sans_ext(image))
+    if (!is.na(match_idx)) image[match_idx] else "0.png"
+  },
+  character(1)
 )
+
 
 tag <- ifelse(is.na(teaching$tag), "TBD", teaching$tag)
 link <- ifelse(is.na(teaching$Link), "", teaching$Link)
@@ -36,6 +40,7 @@ sprintf(
   teaching$Titolo,
   teaching$Docente,
   tag,
+  teaching$description,
   image_to_insert,
   link
 ) |>
